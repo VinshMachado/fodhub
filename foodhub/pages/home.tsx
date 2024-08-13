6f8d47c218e1560d8ff538c14327bd28128cce74
Sion Aaron Lobo <sionaaronlobo@gmail.com>
2024-08-13 22:48:22 +0530

    color pallet home

--- foodhub/pages/home.tsx
+++ foodhub/pages/home.tsx
@@ -2,36 +2,127 @@ import React from "react";
 
 const home = () => {
   return (
-<<<<<<< HEAD
-    <div>
-      <div className="bg-gray-500 h-14 flex items-center justify-between px-4">
-        <form className="relative flex-grow flex items-center max-w-lg mx-auto">
-=======
-    <div className="h-screen w-screen  flex bg-gray-800">
-      <div className=" hidden bg-black w-72 flex items-center m-1  justify-center h-auto rounded-sm  text-white sm:block ">
+    <div className="h-screen w-screen  flex bg-white">
+      <div className="bg-black w-72 flex flex-col items-center m-1 justify-center h-auto rounded-sm text-white sm:block">
+        <div className="flex justify-end w-full px-4 pt-5 hover:text-slate-400">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-8 h-8"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+        </div>
+
         <div className="rounded flex flex-col items-center justify-center space-y-10 mt-48">
-          <button className="bg-zinc-950  w-[13rem] h-10 rounded-lg my-1 font-bold hover:text-black hover:bg-white">
-            home
+          <button className="bg-zinc-950 w-[13rem] h-10 rounded-lg my-1 font-bold hover:text-black hover:bg-white flex items-center justify-center space-x-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+              />
+            </svg>
+            <span>home</span>
           </button>
-          <button className="bg-zinc-950 w-[13rem] h-10 rounded-lg my-1 font-bold hover:bg-white hover:text-black">
-            shop
+
+          <button className="bg-zinc-950 w-[13rem] h-10 rounded-lg my-1 font-bold hover:bg-white hover:text-black flex items-center justify-center space-x-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              />
+            </svg>
+            <span>profile</span>
           </button>
-          <button className="bg-zinc-950 w-[13rem] h-10 rounded-lg my-1 font-bold hover:bg-white hover:text-black">
-            location
+
+          <button className="bg-zinc-950 w-[13rem] h-10 rounded-lg my-1 font-bold hover:bg-white hover:text-black flex items-center justify-center space-x-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+              />
+            </svg>
+            <span>liked</span>
           </button>
-          <button className="bg-zinc-950 w-[13rem] h-10 rounded-lg my-1 font-bold hover:bg-white hover:text-black">
-            you
+
+          <button className="bg-zinc-950 w-[13rem] h-10 rounded-lg my-1 font-bold hover:bg-white hover:text-black flex items-center justify-center space-x-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              />
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
+              />
+            </svg>
+            <span>location</span>
           </button>
-          <div className="h-ful  flex justify-center items-center w-full pt-36">
-            <button className="bg-red-900  w-[13rem] h-10 rounded-lg my-1 font-bold hover:bg-white hover:text-black">
-              logout
+
+          <div className="h-full flex justify-center items-center w-full pt-36">
+            <button className="bg-red-900 w-[13rem] h-10 rounded-lg flex items-center justify-center my-1 font-bold hover:bg-white hover:text-black">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="w-6 h-6 mr-2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15"
+                />
+              </svg>
+              <span>logout</span>
             </button>
           </div>
         </div>
       </div>
-      <div className="bg-gray-950 h-16 w-full  m-1 flex items-center justify-center rounded-sm">
+
+      <div className="bg-orange-400 h-16 w-full  m-1 flex items-center justify-center rounded-sm">
         <form className="w-1/2  sm:w-1/2 mt-1">
->>>>>>> cba122f30c45b99d2dffc98a0738b28cd9899c18
           <label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">
             Search
           </label>
@@ -53,21 +144,6 @@ const home = () => {
                 />
               </svg>
             </div>
-<<<<<<< HEAD
-            <input
-              type="search"
-              id="default-search"
-              className="block w-full h-12 pl-10 pr-20 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Search Mockups, Logos..."
-              required
-            />
-            <button
-              type="submit"
-              className="absolute right-2.5 top-1/2 transform -translate-y-1/2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Search
-            </button>
-=======
             <div className="pt-[2.5px]">
               <input
                 type="search"
@@ -85,18 +161,17 @@ const home = () => {
                 </button>
               </div>
             </div>
->>>>>>> cba122f30c45b99d2dffc98a0738b28cd9899c18
           </div>
         </form>
 
-        <div className="flex items-center">
+        <div className="flex items-center text-white">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
             stroke-width="1.5"
             stroke="currentColor"
-            className="w-8 h-8 text-black mx-4"
+            className="w-8 h-8  mx-4  hover:text-slate-400"
           >
             <path
               stroke-linecap="round"
@@ -111,7 +186,7 @@ const home = () => {
             viewBox="0 0 24 24"
             stroke-width="1.5"
             stroke="currentColor"
-            className="w-8 h-8 text-black mx-4"
+            className="w-8 h-8 mx-4  hover:text-slate-400"
           >
             <path
               stroke-linecap="round"
@@ -121,93 +196,7 @@ const home = () => {
           </svg>
         </div>
       </div>
-<<<<<<< HEAD
-
-      <div>
-        <div className="bg-slate-600 w-[17rem] h-screen pl-5 pt-12 text-white">
-          <div className="rounded flex flex-col space-y-10">
-            <button className="bg-zinc-950 w-[13rem] h-10 rounded-lg my-1 font-bold hover:text-black hover:bg-white flex items-center justify-center space-x-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-                />
-              </svg>
-              <span>home</span>
-            </button>
-
-            <button className="bg-zinc-950 w-[13rem] h-10 rounded-lg my-1 font-bold hover:bg-white hover:text-black flex items-center justify-center space-x-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                />
-              </svg>
-              <span>profile</span>
-            </button>
-
-            <button className="bg-zinc-950 w-[13rem] h-10 rounded-lg my-1 font-bold hover:bg-white hover:text-black flex items-center justify-center space-x-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
-                />
-              </svg>
-              <span>liked</span>
-            </button>
-
-            <button className="bg-zinc-950 w-[13rem] h-10 rounded-lg my-1 font-bold hover:bg-white hover:text-black flex items-center justify-center space-x-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                />
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
-                />
-              </svg>
-              <span>location</span>
-            </button>
-          </div>
-        </div>
-      </div>
-=======
       <div></div>
->>>>>>> cba122f30c45b99d2dffc98a0738b28cd9899c18
     </div>
   );
 };
