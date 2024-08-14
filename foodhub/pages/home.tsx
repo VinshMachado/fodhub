44c83a74d6d444442018d39d9b2279a5e292c775
Sion Aaron Lobo <sionaaronlobo@gmail.com>
2024-08-14 19:51:32 +0530

    pic scrolling thing

--- foodhub/pages/home.tsx
+++ foodhub/pages/home.tsx
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
-const home = () => {
-  //
-  const [expand, setexpand] = useState(true);
-  const [size, setsize] = useState(
-    "hidden bg-black  flex flex-col items-center w-72  justify-center h-auto  text-white sm:block"
+const Home = () => {
+  const [expand, setExpand] = useState(true);
+  const [size, setSize] = useState(
+    "hidden bg-black flex flex-row items-center w-72 justify-center h-auto text-white sm:block"
   );
 
-  const [iconname, seticonname] = useState([
+  const [iconName, setIconName] = useState([
     "Home",
     "Location",
     "liked",
@@ -15,39 +21,39 @@ const home = () => {
     "Logout",
   ]);
 
-  const expandthing = () => {
-    setexpand(!expand);
-    if (expand == true) {
-      setsize(
-        " hidden bg-black  flex flex-col items-center w-72  justify-center h-auto  text-white sm:block"
+  const expandThing = () => {
+    setExpand(!expand);
+    if (expand) {
+      setSize(
+        "hidden bg-black flex flex-col items-center w-72 justify-center h-auto text-white sm:block"
       );
-      seticonname(["Home", "Location", "liked", "Profile", "Logout"]);
+      setIconName(["Home", "Location", "liked", "Profile", "Logout"]);
     } else {
-      setsize(
-        "hidden bg-black  flex flex-col items-center w-20  justify-center h-auto  text-white sm:block"
+      setSize(
+        "hidden bg-black flex flex-col items-center w-20 justify-center h-auto text-white sm:block"
       );
-      seticonname(["", "", "", "", ""]);
+      setIconName(["", "", "", "", ""]);
     }
   };
 
   return (
-    <div className="h-screen w-screen  flex bg-white">
+    <div className="h-screen w-screen bg-white flex">
       <div className={`${size}`}>
         <div
-          className="flex justify-start w-full px-4 pt-5 hover:text-slate-400"
-          onClick={expandthing}
+          className="flex justify-start w-[32px] ml-5 pt-5 hover:text-slate-400 cursor-pointer"
+          onClick={expandThing}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
             className="w-8 h-8"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
             />
           </svg>
@@ -69,7 +75,7 @@ const home = () => {
                 d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
               />
             </svg>
-            <span>{iconname[0] /*home */}</span>
+            <span>{iconName[0] /*home */}</span>
           </button>
 
           <button className="bg-zinc-950 w-full h-10 rounded-lg my-1 font-bold hover:bg-white hover:text-black flex items-center justify-center space-x-2">
@@ -87,7 +93,7 @@ const home = () => {
                 d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
               />
             </svg>
-            <span>{iconname[1] /*location */}</span>
+            <span>{iconName[1] /*location */}</span>
           </button>
 
           <button className="bg-zinc-950 w-full h-10 rounded-lg my-1 font-bold hover:bg-white hover:text-black flex items-center justify-center space-x-2">
@@ -105,7 +111,7 @@ const home = () => {
                 d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
               />
             </svg>
-            <span>{iconname[2] /*liked */}</span>
+            <span>{iconName[2] /*liked */}</span>
           </button>
 
           <button className="bg-zinc-950 w-full h-10 rounded-lg my-1 font-bold hover:bg-white hover:text-black flex items-center justify-center space-x-2">
@@ -128,7 +134,7 @@ const home = () => {
                 d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
               />
             </svg>
-            <span>{iconname[3] /*Profile */}</span>
+            <span>{iconName[3] /*Profile */}</span>
           </button>
 
           <div className="h-full flex justify-center items-center w-full pt-20">
@@ -147,89 +153,105 @@ const home = () => {
                   d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15"
                 />
               </svg>
-              <span>{iconname[4] /*Logout */}</span>
+              <span>{iconName[4] /*Logout */}</span>
             </button>
           </div>
         </div>
       </div>
 
-      <div className="bg-orange-400 h-16 w-full   flex items-center justify-center ">
-        <form className="w-1/2  sm:w-1/2 mt-1">
-          <label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">
-            Search
-          </label>
-          <div className="relative flex-grow">
-            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-              <svg
-                className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+      {/* Main content area */}
+      <div className="flex-1 bg-gray-100">
+        <div className="bg-orange-400 h-16 w-full flex items-center justify-center">
+          <form className="w-1/2 sm:w-1/2 mt-1">
+            <label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">
+              Search
+            </label>
+            <div className="relative flex-grow">
+              <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"></div>
+              <div className="pt-[2.5px]">
+                <input
+                  type="search"
+                  id="default-search"
+                  className="block w-full h-12 p-4 text-lg text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Search"
+                  required
                 />
-              </svg>
-            </div>
-            <div className="pt-[2.5px]">
-              <input
-                type="search"
-                id="default-search"
-                className="block w-full h-12 p-4 ps-10 text-lg text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Search "
-                required
-              />
-              <div className="pt-1">
-                <button
-                  type="submit"
-                  className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                  Search
-                </button>
+                <div className="pt-1">
+                  <button
+                    type="submit"
+                    className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full border-2 text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    <svg
+                      className="w-5 h-5 text-black"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 20 20"
+                    >
+                      <path
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                      />
+                    </svg>
+                  </button>
+                </div>
               </div>
             </div>
+          </form>
+
+          <div className="flex items-center text-white">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-8 h-8 mx-4 hover:text-slate-400"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
+              />
+            </svg>
+
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-8 h-8 mx-4 hover:text-slate-400"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              />
+            </svg>
           </div>
-        </form>
+        </div>
 
-        <div className="flex items-center text-white">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            className="w-8 h-8  mx-4  hover:text-slate-400"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
-            />
-          </svg>
-
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            className="w-8 h-8 mx-4  hover:text-slate-400"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-            />
-          </svg>
+        {/* New "Hello" div */}
+        <div className=" w-full top-16 bg-slate-500 p-4">
+          <div className="text-4xl font-bold">
+            <Carousel>
+              <CarouselContent>
+                <CarouselItem>biryani</CarouselItem>
+                <CarouselItem>butterchicken</CarouselItem>
+                <CarouselItem>kebab</CarouselItem>
+              </CarouselContent>
+              <CarouselPrevious />
+              <CarouselNext />
+            </Carousel>
+          </div>
         </div>
       </div>
     </div>
   );
 };
 
-export default home;
+export default Home;
