5e8a69c02805afa1c5872564f26d2fdf18e552ef
Sion Aaron Lobo <sionaaronlobo@gmail.com>
2024-08-16 19:27:29 +0530

    home page

--- foodhub/pages/home.tsx
+++ foodhub/pages/home.tsx
@@ -12,7 +12,7 @@ import Modal from "@/components/ui/model";
 const Home = () => {
   const [expand, setExpand] = useState(true);
   const [size, setSize] = useState(
-    "hidden bg-black flex flex-row items-center w-72 justify-center h-auto text-white sm:block"
+    "snap-center hidden bg-black flex flex-row items-center w-72 justify-center h-auto text-white sm:block"
   );
 
   const [iconName, setIconName] = useState([
@@ -39,7 +39,7 @@ const Home = () => {
   };
 
   return (
-    <div className="h-screen w-screen bg-white flex">
+    <div className="h-full w-screen bg-white flex">
       <div className={`${size}`}>
         <div
           className="flex justify-start w-[32px] ml-5 pt-5 hover:text-slate-400 cursor-pointer "
@@ -252,6 +252,111 @@ const Home = () => {
                 <CarouselItem>raul </CarouselItem>
                 <CarouselItem>is</CarouselItem>
                 <CarouselItem>gay</CarouselItem>
+              </CarouselContent>
+              <CarouselPrevious />
+              <CarouselNext />
+            </Carousel>
+          </div>
+        </div>
+
+        <div className="mt-5 h-` w-11/12 flex justify-center  items-end rounded-sm  top-16 bg-slate-500 p-4 ">
+          <div className="  w-11/12 md:text-4xl font-bold text-lg">
+            <Carousel>
+              <CarouselContent className="text-white ">
+                <CarouselItem className=" basis-1/3 justify-evenly">
+                  chicken{" "}
+                </CarouselItem>
+                <CarouselItem className="basis-1/3"> pijja</CarouselItem>
+                <CarouselItem className="basis-1/3">burgir</CarouselItem>
+                <CarouselItem className="basis-1/3">biryani</CarouselItem>
+                <CarouselItem className="basis-1/3">
+                  {" "}
+                  butter chicken
+                </CarouselItem>
+              </CarouselContent>
+              <CarouselPrevious />
+              <CarouselNext />
+            </Carousel>
+          </div>
+        </div>
+        <div className="pt-3 text-4xl font-bold">Chinese Restraunts</div>
+        <div className="mt-4 h-64 w-11/12 flex justify-center items-center  md:text-4xl text-lg rounded-sm  top-16 bg-yellow-700 late-500 p-4">
+          <div className="  w-11/12 font-bold">
+            <Carousel>
+              <CarouselContent className="text-white">
+                <CarouselItem className=" basis-1/3 ">
+                  Chinese restraunt 1{" "}
+                </CarouselItem>
+                <CarouselItem className="basis-1/3">
+                  {" "}
+                  Chinese restraunt 2
+                </CarouselItem>
+                <CarouselItem className="basis-1/3">
+                  Chinese restraunt 3
+                </CarouselItem>
+                <CarouselItem className="basis-1/3">
+                  Chinese restraunt 4
+                </CarouselItem>
+                <CarouselItem className="basis-1/3">
+                  {" "}
+                  butter chicken
+                </CarouselItem>
+              </CarouselContent>
+              <CarouselPrevious />
+              <CarouselNext />
+            </Carousel>
+          </div>
+        </div>
+        <div className="pt-3 text-4xl font-bold">North Indian Restraunts</div>
+        <div className="mt-4 h-64 w-11/12 flex justify-center items-center rounded-sm   md:text-4xl text-lg top-16 bg-orange-500 p-4">
+          <div className="  w-11/12 text-4xl font-bold">
+            <Carousel>
+              <CarouselContent className="text-white">
+                <CarouselItem className=" basis-1/3 ">
+                  north indian 1{" "}
+                </CarouselItem>
+                <CarouselItem className="basis-1/3">
+                  {" "}
+                  north indian 2
+                </CarouselItem>
+                <CarouselItem className="basis-1/3">
+                  north indian 3
+                </CarouselItem>
+                <CarouselItem className="basis-1/3">
+                  north indian 4
+                </CarouselItem>
+                <CarouselItem className="basis-1/3">
+                  {" "}
+                  butter chicken
+                </CarouselItem>
+              </CarouselContent>
+              <CarouselPrevious />
+              <CarouselNext />
+            </Carousel>
+          </div>
+        </div>
+        <div className="pt-3 text-4xl font-bold">South Indian restraunts</div>
+        <div className="mt-4 h-64 w-11/12 flex justify-center items-center rounded-sm  md:text-4xl text-lg top-16 bg-black p-4">
+          <div className="  w-11/12 text-4xl font-bold">
+            <Carousel>
+              <CarouselContent className="text-white">
+                <CarouselItem className=" basis-1/3 ">
+                  south indian 1{" "}
+                </CarouselItem>
+                <CarouselItem className="basis-1/3">
+                  {" "}
+                  south indian 2
+                </CarouselItem>
+                <CarouselItem className="basis-1/3">
+                  south indian 3
+                </CarouselItem>
+                <CarouselItem className="basis-1/3">
+                  south indian 4
+                </CarouselItem>
+                <CarouselItem className="basis-1/3">
+                  {" "}
+                  butter chicken
+                </CarouselItem>
               </CarouselContent>
               <CarouselPrevious />
               <CarouselNext />
