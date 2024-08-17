5446988c4bb4452a878a62c4159becb61ae68c22
Sion Aaron Lobo <sionaaronlobo@gmail.com>
2024-08-17 21:11:28 +0530

    hotel page

--- foodhub/pages/hotel.tsx
+++ foodhub/pages/hotel.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Maincontent from "@/components/maincontent";
+import Car from "@/components/ui/car";
 import {
   Carousel,
   CarouselContent,
@@ -9,33 +10,17 @@ import {
 } from "@/components/ui/carousel";
 import Profilething from "@/components/ui/Profilething";
 import Modal from "@/components/ui/model";
-import Car from "@/components/ui/car";
 
 const Hotel = () => {
   return (
-<<<<<<< HEAD
     <div>
-      <span className="font-bold text-4xl justify-center flex items-center mt-4">
-        name of hotel
+      <span className="text-4xl font-bold flex justify-center items-center mt-3">
+        bombay lucky
       </span>
-      <div className="flex justify-center items-center ">
+      <div className="flex justify-center items-center">
         <Car />
-=======
-    <div className="flex flex-col h-screen w-full items-center">
-      <div className="mt-5 h-56 flex justify-center w-11/12 items-center bg-slate-500 p-4 rounded-sm">
-        <div className=" h-full w-11/12 text-4xl font-bold ">
-          <Carousel className="h-full w-full  flex items-end">
-            <CarouselContent className="text-white">
-              <CarouselItem>Food</CarouselItem>
-              <CarouselItem>Place</CarouselItem>
-              <CarouselItem>Price</CarouselItem>
-            </CarouselContent>
-            <CarouselPrevious />
-            <CarouselNext />
-          </Carousel>
-        </div>
->>>>>>> ec0c04f82fcbbf91f42d6a30200e1c74162b5953
       </div>
+      <div></div>
     </div>
   );
 };
