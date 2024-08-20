165da6e04851e8335b856d1b712070872f68b206
VinishMachado <155185076+VinshMachado@users.noreply.github.com>
2024-08-20 20:38:14 +0530

    dont push

--- foodhub/pages/index.tsx
+++ foodhub/pages/index.tsx
@@ -1,17 +1,25 @@
-import { useRouter } from "next/router";
-
+import React, { useEffect, useState, useContext, createContext } from "react";
+import Maincontent from "@/components/maincontent";
+import { Searchbar } from "@/components/ui/Searchbar";
+import Sidebar from "@/components/ui/sidebar";
 const Home = () => {
-  const router = useRouter();
-
-  const navigateToAbout = () => {
-    router.push("/login");
-  };
+  //nav bar logic///
 
   return (
-    <div>
-      <h1>Home Page</h1>
-      <button onClick={navigateToAbout}>Login</button>
+    <div className="h-screen w-screen bg-white flex overflow-x-hidden overflow-y-hidden">
+      <Sidebar />
+      <div className="h-full w-full">
+        <div className="h-full w-fullse">
+          <div className="h-20 w-full ">
+            <Searchbar />
+          </div>
+          <div className="h-full w-full overflow-auto">
+            <Maincontent />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
+
 export default Home;
